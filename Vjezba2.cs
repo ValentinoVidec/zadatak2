6900e35a13e58d6f58b1452f75a4977dd1859cd8
ValentinoVidec <valentinovidec1505@gmail.com>
2024-12-09 12:46:29 +0100

    Add files via upload

---
+++ Vjezba2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int year = 1900; year <= 2100; year++)
+            {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    Console.WriteLine(year);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
